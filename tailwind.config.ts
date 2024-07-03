d9abcea5841a99f28dbeafd6a1b8af0f34f6c8f5
Themuuln <zerone.offical@gmail.com>
2024-07-03 17:18:10 +0800

    updated utils

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,14 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: '#174a8b',
-        accent: '#bde1ff',
-        background: '#483eff',
+        primary: '#001644',
+        accent: '#BDE1FF',
+        background: '#483EFF',
+        muted: '#ADB5BD',
+        field: '#f8f9fe',
+        Arcade: '#FEAF7E',
+        Advanced: '#FD8392',
+        Pro: '#483EFE',
       },
     },
   },
